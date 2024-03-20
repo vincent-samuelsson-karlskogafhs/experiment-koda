7d34c2e97c76e9cabea59fcba11ea20ac5c1ec71
Master Vincent <vincent.samuelsson@hotmail.com>
2024-03-20 13:53:14 +0100

    added

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 const button = document.querySelector(".button");
-const inputText = document.querySelector(".input-text");
-
-const handleClick = () => {
-  inputText.value = "Input Tepegwt";
+const removeCircle = (document.querySelector = () => {
+  const redCirle = document.querySelector(".red");
+  redCircle.parentNode.removeChild(redCircle);
 };
-console.log(handleClick);
-handleClick();
-button.addEventListener("click", handleClick);
-console.log();
+
+document.querySelector('#wrapper input').setAttribute('id', 'inputEl');
