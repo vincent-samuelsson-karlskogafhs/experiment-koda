fff5f84e8214ae50f71f6471fedf783d72009736
Master Vincent <vincent.samuelsson@hotmail.com>
2024-03-21 14:47:32 +0100

    added

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-const button = document.querySelector("#button");
-console.log("workíng", button);
-const wrapper = document.createElement("div");
+// skapa -> vart ska den läggas
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+  const button = document.querySelector(".button");
 
-console.log("div working", wrapper);
+  const firstName = document.querySelector(".first-name");
+
+  const lastName = document.querySelector(".last-name");
+
+  const fullName = document.querySelector(".full-name");
+
+  const verifyCode = document.querySelector(".verify-code");
+
+  fullName.value = firstName.value + "" + lastName.value;
+
+  console.log("first name", firstName);
+  console.log("last name", lastName);
+});
