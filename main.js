c4079e3895aa1a696e862d8d93a1fa73460a52d3
Master Vincent <vincent.samuelsson@hotmail.com>
2024-03-25 13:51:15 +0100

    added

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const input = document.querySelector(".list");
+const input = document.querySelector(".input");
 
-input.addEventListener(".input", () => {
+input.addEventListener("input", () => {
   const listItems = document.querySelectorAll(".list li");
   listItems.forEach((li) => {
-    if (li.innerText.includes(input.value)) return (li.hidden = false);
-    return (li.hidden = true);
+    if (li.innerText.includes(input.value)) {
+      li.hidden = false;
+    } else {
+      li.hidden = true;
+    }
   });
 });
 
