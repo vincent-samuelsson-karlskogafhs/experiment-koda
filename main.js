812989844c5ec6645910c5747ae1835798557826
Master Vincent <vincent.samuelsson@hotmail.com>
2024-03-25 13:29:50 +0100

    added

--- main.js
+++
@@ -1,7 +0,0 @@
-// skapa en map  - sedan skapar du vart den ska läggas
-const button = document.querySelector(".button");
-button.addEventListener("click", () => {
-  let Oldvalue = parseInt(button.innerText, 10);
-  button.innerText = Oldvalue + 1;
-});
-console.log("working", button);
